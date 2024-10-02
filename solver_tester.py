375bf32bcf9f75b432aed74257c52c7b75893718
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2024-10-02 15:03:41 +0800

    API准确化

--- solver_tester.py
+++ solver_tester.py
@@ -7,6 +7,13 @@ from pino_ik_solver import PinoIkSolver, XyzQuat
 quat = R.from_euler('xyz', [0.1, 0.2, 0.3]).as_quat()
 
 svr = PinoIkSolver('[sample]')
-print(svr.solve_ik('shoulder1_joint', 'wrist2_joint',
-    (np.array([1., 0., 1.]), quat), None
+print(
+    svr.solve_ik(
+        'shoulder1_joint',
+        'wrist2_joint',
+        # target
+        'shoulder1_joint',
+        'wrist2_joint',
+        (np.array([1., 0., 1.]), quat),
+        None
 ))
